a513088b1485ea3226544029c529e5020b5bea95
olegvantsevich <OVantsevich@gmail.com>
2023-02-08 11:51:41 +0300

    08.02.2023 kafka

---
+++ KafkaTest/go.mod
@@ -0,0 +1,22 @@
+module KafkaTest
+
+go 1.19
+
+require (
+	github.com/segmentio/kafka-go v0.4.38
+	github.com/sirupsen/logrus v1.9.0
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgx/v5 v5.2.0 // indirect
+	github.com/jackc/puddle/v2 v2.1.2 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	golang.org/x/sync v0.0.0-20220923202941-7f9b1623fab7 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/text v0.3.8 // indirect
+)
---
+++ KafkaTest/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"strconv"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	topic := "my-topic"
+	partition := 0
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+	if err != nil {
+		logrus.Fatal("failed to dial leader:", err)
+	}
+
+	var msg [2000]kafka.Message
+
+	for i := range msg {
+		msg[i] = kafka.Message{Value: []byte(strconv.Itoa(i))}
+	}
+
+	conn.SetWriteDeadline(time.Now().Add(1 * time.Second))
+	_, err = conn.WriteMessages(
+		msg[0:]...,
+	)
+	if err != nil {
+		logrus.Fatal("failed to write messages:", err)
+	}
+
+	ch := make(chan int, 1)
+	go consumer(ch)
+
+	if err := conn.Close(); err != nil {
+		logrus.Fatal("failed to close writer:", err)
+	}
+
+	<-ch
+}
+
+func consumer(ch chan int) {
+	topic := "my-topic"
+	partition := 0
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+	if err != nil {
+		logrus.Fatal("failed to dial leader:", err)
+	}
+
+	conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+	batch := conn.ReadBatch(10e3, 1e6)
+
+	b := make([]byte, 10e4)
+	pgxBatch := &pgx.Batch{}
+	for {
+		n, err := batch.Read(b)
+		if err != nil {
+			break
+		}
+		num := string(b[:n])
+		i, _ := strconv.Atoi(num)
+		pgxBatch.Queue("INSERT INTO users(number) VALUES ($1)", i)
+		fmt.Println(num)
+	}
+
+	postgresPool, err := pgxpool.New(context.Background(), fmt.Sprintf("postgres://postgres:postgres@localhost:5432/postgres"))
+	if err != nil {
+		logrus.Fatalf("Could not connect to db %s", err)
+	}
+	defer postgresPool.Close()
+
+	batchResults := postgresPool.SendBatch(context.Background(), pgxBatch)
+	err = batchResults.Close()
+
+	if err := batch.Close(); err != nil {
+		logrus.Fatal("failed to close batch:", err)
+	}
+
+	if err := conn.Close(); err != nil {
+		logrus.Fatal("failed to close connection:", err)
+	}
+
+	ch <- 1
+}
